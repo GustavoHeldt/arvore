4d8a2e3f6fa6eee448ed69646605266b228713de
rh_cefet <rh_cefet@yahoo.com.br>
2013-04-12 16:21:51 +0000

    import incial

---
+++ arvore/src/TesteBinariaPesquisa.java
@@ -0,0 +1,25 @@
+
+public class TesteBinariaPesquisa {
+	
+	public static void main(String[] args) {
+		
+		ArvorePesquisaBinaria abp = new ArvorePesquisaBinaria();
+		
+		abp.inserir(6);
+		abp.inserir(2);
+		abp.inserir(1);
+		abp.inserir(4);
+		abp.inserir(3);
+		abp.inserir(5);
+		abp.inserir(9);
+		abp.inserir(8);
+		
+		abp.preOrdem(abp.raiz);
+		System.out.println("\n");
+		abp.remover(2);
+		
+		abp.preOrdem(abp.raiz);
+		
+	}
+
+}
