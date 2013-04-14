d5e785ba7152eff2af12f9df6cc9654f0353be19
rh_cefet <rh_cefet@yahoo.com.br>
2013-04-14 03:22:11 +0000

    implementando próximo do correto, os métodos de inserir e remover

--- arvore/src/IArvoreAVL.java
+++ arvore/src/IArvoreAVL.java
@@ -1,9 +1,9 @@
 public interface IArvoreAVL extends IArvore{
 
-	public abstract void rotacaoDireita(INoAVL no);
-	public abstract void rotacaoEsquerda(INoAVL no);
+	public abstract void rotacaoDireita(INo no);
+	public abstract void rotacaoEsquerda(INo no);
 
-	public abstract void rotacaoDuplaDireita(INoAVL no);
-	public abstract void rotacaoDuplaEsquerda(INoAVL no);
+	public abstract void rotacaoDuplaDireita(INo no);
+	public abstract void rotacaoDuplaEsquerda(INo no);
 
 }
