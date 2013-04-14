8c4f448fe0321a19345b4b7983ea0db5638ab35a
rh_cefet@yahoo.com.br <rh_cefet@yahoo.com.br@95db7adc-ed30-8140-7b0f-43ac7debb2d5>
2013-04-14 03:22:11 +0000

    implementando próximo do correto, os métodos de inserir e remover

--- trunk/arvore/src/ArvoreAVL.java
+++ trunk/arvore/src/ArvoreAVL.java
@@ -1,18 +1,61 @@
 
 public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 
+	public ArvoreAVL() {
+		raiz = new NoAVL();
+		raiz.setFilhoEsquerdo(new NoAVL());
+	}
+
+	
 	public void inserir(Object chave) {
 
-		((IArvoreAVL) this).inserir(chave);
-		this.buscar(chave);
+		INo no = this.buscar(chave);
+
+		this.inserir(no, chave);
+
+		INo nD = new NoAVL();
+		no.setFilhoDireito(nD);
+		nD.setPai(no);
+
+		INo nE = new NoAVL();
+		no.setFilhoEsquerdo(nE);
+		nE.setPai(no);
+		
+		int dif = 0;
+		if (no.getPai().getFilhoEsquerdo() == no) {
+			dif = 1;
+		} else {
+			dif = -1;
+		}
+		
+		atualizarFBInsercao((INoAVL) no, dif);
 	}
 	
 	
-	public void atualizarFBInsercao(INoAVL noInserido, int dif) {
+	public INo remover(Object chave) {
+
+		INo no = buscar(chave);
+		remover(no);
+		
+		int dif = 0;
+		if (no.getPai().getFilhoEsquerdo() == no) {
+			dif = -1;
+		} else {
+			dif = 1;
+		}
+		
+		atualizarFBRemocao((INoAVL) no, dif);
+		
+		return no;
+	}
+	
+	
+	private void atualizarFBInsercao(INoAVL noInserido, int dif) {
 		INoAVL noPai = (INoAVL) noInserido.getPai();
 		noPai.setFB(noPai.getFB() + dif);
 		
 		if (noPai.getFB() == -2) {
+			//codigo repetido 2
 			if (noInserido.getFB() <= 0) {
 				rotacaoEsquerda(noPai);
 			} else {
@@ -20,6 +63,7 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 			}
 			
 		} else if (noPai.getFB() == 2) {
+			//codigo repetido 2
 			if (noInserido.getFB() >= 0) {
 				rotacaoDireita(noPai);
 			} else {
@@ -33,9 +77,9 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 	}
 	
 	
-	public void atualizarFBRemocao(INoAVL no, int dif) {
+	private void atualizarFBRemocao(INoAVL no, int dif) {
 		
-		INoAVL noFilho;
+		INoAVL noFilho = null;
 		no.setFB(no.getFB() + dif);
 		
 		if (dif == -1) {
@@ -45,14 +89,16 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 		}
 		
 		if (no.getFB() == -2) {
-			if (no.getFB() <= 0) {
+			//codigo repetido 2
+			if (noFilho.getFB() <= 0) {
 				rotacaoEsquerda(no);
 			} else {
 				rotacaoDuplaEsquerda(no);
 			}
 			
 		} else if (no.getFB() == 2) {
-			if (no.getFB() >= 0) {
+			//codigo repetido 2
+			if (noFilho.getFB() >= 0) {
 				rotacaoDireita(no);
 			} else {
 				rotacaoDuplaDireita(no);
@@ -66,11 +112,11 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 	}
 
 	@Override
-	public void rotacaoDireita(INoAVL no) {
+	public void rotacaoDireita(INo no) {
 		
-		INoAVL noPai = (INoAVL) no.getPai();
-		INoAVL noNeto = (INoAVL) no.getFilhoDireito();
-		INoAVL noFilho = (INoAVL) no.getFilhoEsquerdo();
+		INo noPai = no.getPai();
+		INo noNeto = no.getFilhoDireito();
+		INo noFilho = no.getFilhoEsquerdo();
 		
 		//codigo repetido 1
 		if (no == noPai.getFilhoDireito()) {
@@ -86,6 +132,16 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 		no.setFilhoEsquerdo(noNeto);
 		noNeto.setPai(no);
 
+
+	}
+	
+	private void rotacaoDireita(INoAVL no) {
+		INoAVL noPai = (INoAVL) no.getPai();
+		INoAVL noNeto = (INoAVL) no.getFilhoDireito();
+		INoAVL noFilho = (INoAVL) no.getFilhoEsquerdo();
+		
+		rotacaoDireita(no);
+		
 		//atualiza o FB dos nós envolvidos
 		noFilho.setFB(calculaFB(noFilho));
 		no.setFB(calculaFB(no));
@@ -93,11 +149,11 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 	}
 
 	@Override
-	public void rotacaoEsquerda(INoAVL no) {
+	public void rotacaoEsquerda(INo no) {
 		
-		INoAVL noPai = (INoAVL) no.getPai();
-		INoAVL noSucessor = (INoAVL) this.sucessor(no);
-		INoAVL noFilho = (INoAVL) no.getFilhoDireito();
+		INo noPai = no.getPai();
+		INo noSucessor = this.sucessor(no);
+		INo noFilho = no.getFilhoDireito();
 		
 		//codigo repetido 1
 		if (no == noPai.getFilhoDireito()) {
@@ -115,7 +171,16 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 		}
 		no.setFilhoDireito(noSucessor);
 		noSucessor.setPai(no);
-
+	}
+	
+	private void rotacaoEsquerda(INoAVL no) {
+		
+		INoAVL noPai = (INoAVL) no.getPai();
+		INoAVL noSucessor = (INoAVL) this.sucessor(no);
+		INoAVL noFilho = (INoAVL) no.getFilhoDireito();
+		
+		rotacaoEsquerda(no);
+		
 		//atualiza o FB dos nós envolvidos
 		noFilho.setFB(calculaFB(noFilho));
 		no.setFB(calculaFB(no));
@@ -123,13 +188,24 @@ public class ArvoreAVL extends ArvoreBP implements IArvoreAVL {
 	}
 
 	@Override
-	public void rotacaoDuplaDireita(INoAVL no) {
-		rotacaoDireita((INoAVL) no.getFilhoEsquerdo());
+	public void rotacaoDuplaDireita(INo no) {
+		rotacaoDireita(no.getFilhoEsquerdo());
 		rotacaoEsquerda(no);
 	}
 
 	@Override
-	public void rotacaoDuplaEsquerda(INoAVL no) {
+	public void rotacaoDuplaEsquerda(INo no) {
+		rotacaoDireita(no.getFilhoDireito());
+		rotacaoEsquerda(no);
+	}
+	
+	
+	private void rotacaoDuplaDireita(INoAVL no) {
+		rotacaoDireita((INoAVL) no.getFilhoEsquerdo());
+		rotacaoEsquerda(no);
+	}
+
+	private void rotacaoDuplaEsquerda(INoAVL no) {
 		rotacaoDireita((INoAVL) no.getFilhoDireito());
 		rotacaoEsquerda(no);
 	}
